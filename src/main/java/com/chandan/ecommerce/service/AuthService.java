90e6046408de8cd57fb6e05b501b74e14431a3fd
Chandan <chandankuma8001360@gmail.com>
2026-01-06 00:19:13 +0530

    otp generator

--- src/main/java/com/chandan/ecommerce/service/AuthService.java
+++ src/main/java/com/chandan/ecommerce/service/AuthService.java
@@ -3,5 +3,6 @@ package com.chandan.ecommerce.service;
 import com.chandan.ecommerce.response.SignupRequest;
 
 public interface AuthService {
+    void sentLoginOtp(String email) throws Exception;
     String createUser(SignupRequest req) throws Exception;
 }
